63958825a504089fb1805d066731611af41f0596
Vijay <jobprepnyu@gmail.com>
2020-07-28 23:38:18 -0400

    Starting touches

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,13 +9,80 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer, dice;
+var score, roundScore, activePlayer;
 
 scores = [0,0]; 
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random()*6)+1;
-console.log(dice)
 
-document.querySelector('#current-' + activePlayer).textContent = dice
+
+
+
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1 . We need a random number
+    var dice = Math.floor(Math.random()*6)+1;
+
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block'
+    diceDom.src = 'dice-' + dice + '.png'; 
+
+    if (dice !== 1){
+
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else{
+        //Do something
+        activePlayer === 0? activePlayer = 1 : activePlayer = 0 
+        roundScore = 0
+
+        document.getElementById('current-0').textContent = '0'
+        document.getElementById('current-1').textContent = '0'
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        diceDom.style.display = 'none'
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
