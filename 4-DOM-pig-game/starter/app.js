7e1775a52b91279ef11f989b45f160f8f789c858
Vijay <jobprepnyu@gmail.com>
2020-08-05 00:16:15 -0400

    First Draft

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,39 +9,65 @@ GAME RULES:
 
 */
 
-var score, roundScore, activePlayer;
-
-scores = [0,0]; 
-roundScore = 0;
-activePlayer = 0;
+var score, roundScore, activePlayer, isTheGameActive;
 
 
 
+initializeGame()
 
-
-
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
-
+var diceDom = document.querySelector('.dice');
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     //1 . We need a random number
-    var dice = Math.floor(Math.random()*6)+1;
+    if (isTheGameActive){
+        var dice = Math.floor(Math.random()*6)+1;
 
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block'
-    diceDom.src = 'dice-' + dice + '.png'; 
-
-    if (dice !== 1){
-
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    
+        diceDom.style.display = 'block'
+        diceDom.src = 'dice-' + dice + '.png'; 
+    
+        if (dice !== 1){
+    
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        
+        }
+        else{
+            //Do something
+            nextPlayer()
+            
+        }
     }
-    else{
-        //Do something
-        activePlayer === 0? activePlayer = 1 : activePlayer = 0 
+    
+});
+
+document.querySelector('.btn-new').addEventListener('click', initializeGame)
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (isTheGameActive){
+        scores[activePlayer] += roundScore
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+        if (scores[activePlayer] >= 20){
+            isTheGameActive = false
+            document.querySelector('#name-'+activePlayer).textContent = "Winner!";
+            diceDom.style.display = 'none'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    
+        }
+        else{
+            nextPlayer()
+        }
+    }
+    
+  
+
+    //Check if the dude won or not
+
+});
+
+function nextPlayer() {
+    activePlayer === 0? activePlayer = 1 : activePlayer = 0 
         roundScore = 0
 
         document.getElementById('current-0').textContent = '0'
@@ -50,16 +76,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
         diceDom.style.display = 'none'
-    }
-});
+}
 
-document.querySelector('.btn-hold').addEventListener('click', function() {
+function initializeGame(){
+    isTheGameActive = true
+    scores = [0,0]; 
+    roundScore = 0;
+    activePlayer = 0;
+    
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
 
-});
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
 
-
-
-
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    
+    document.getElementById('name-0').textContent = "Player 1";
+    document.getElementById('name-1').textContent = "Player 2";
+}
 
 
 
